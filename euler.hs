ed673c9921c2973f78bab2572074c6d98182fa7c
Prasenjit <cprasenj@thoughtworks.com>
2016-01-13 16:53:49 +0530

    Euler second problem<Prasenjit>

--- euler.hs
+++ euler.hs
@@ -1,16 +1,31 @@
 module Euler where
 
 multipleOf3Or5 :: Integer -> Bool
-multipleOf3Or5 a = if (a `mod` 3 == 0) || (a `mod` 5 == 0)
-                        then True
-                        else False
+multipleOf3Or5 a =
+    if (a `mod` 3 == 0) || (a `mod` 5 == 0)
+        then True
+        else False
 
 multipleOf17Or19 :: Integer -> Bool
-multipleOf17Or19 a = if (a `mod` 17 == 0) || (a `mod` 19 == 0)
-                        then True
-                        else False
+multipleOf17Or19 a =
+    if (a `mod` 17 == 0) || (a `mod` 19 == 0)
+        then True
+        else False
 
 sumOfMultiPlesInAGivenRangeUnderACondition :: Integer -> Integer -> (Integer -> Bool) -> Integer
-sumOfMultiPlesInAGivenRangeUnderACondition first second predicate = if first > second
-                                                                        then 0
-                                                                        else foldl (+) 0 (filter predicate [first..second])
+sumOfMultiPlesInAGivenRangeUnderACondition first second predicate =
+    if first > second
+        then 0
+        else foldl (+) 0 (filter predicate [first..second])
+
+fibonachi :: Integer -> Integer -> Integer ->[Integer]
+fibonachi first second limit =
+    if first > second || limit <= first
+        then []
+    else [first] ++ fibonachi second (first + second) limit
+
+isEven :: Integer -> Bool
+isEven number = number `mod` 2 == 0
+
+createSumOfFilteredList :: Integer -> Integer -> Integer -> (Integer -> Integer-> Integer -> [Integer]) -> (Integer -> Bool) -> Integer
+createSumOfFilteredList start end limit listCreator predicate  = foldl (+) 0 (filter predicate (listCreator start end limit))
--- eulerTest.hs
+++ eulerTest.hs
@@ -38,3 +38,36 @@ main = hspec $ do
 
         it "should give 36 for 0 19 multipleOf3Or5" $ do
             sumOfMultiPlesInAGivenRangeUnderACondition 0 19  multipleOf17Or19 `shouldBe` 36
+
+    describe "fibonachi" $ do
+
+        it "should give empty list for wrong argument" $ do
+            fibonachi 3 2 1 `shouldBe` []
+
+        it "should give empty list for wrong argument" $ do
+            fibonachi 3 2 (-1) `shouldBe` []
+
+        it "should give [0] for 0 1 1" $ do
+            fibonachi 0 1 2 `shouldBe` [0, 1, 1]
+
+        it "should give [0,1] for 0 1 2" $ do
+            fibonachi 0 1 2 `shouldBe` [0, 1, 1]
+
+        it "should give [0, 1, 1, 2, 3, 5, 8] for 0 1 10" $ do
+            fibonachi 0 1 10 `shouldBe` [0, 1, 1, 2, 3, 5, 8]
+
+    describe "isEven" $ do
+
+        it "should give True for 4" $ do
+            isEven 4 `shouldBe` True
+
+        it "should give False for 4" $ do
+            isEven 5 `shouldBe` False
+
+    describe "createSumOfFilteredList" $ do
+
+        it "should give 2 for 1 2 2 fibonachi isEven" $ do
+            createSumOfFilteredList 1 2 3 fibonachi isEven `shouldBe` 2
+
+        it "should give 4613732 for 1 2 2 fibonachi isEven" $ do
+            createSumOfFilteredList 1 2 3999999 fibonachi isEven `shouldBe` 4613732
