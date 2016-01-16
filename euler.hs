c92b655d61d32c1d2d04b7af2c45c257743d7784
Prasenjit <cprasenj@thoughtworks.com>
2016-01-16 17:47:19 +0530

    project euler problem 8<Prasenjit>

--- euler.hs
+++ euler.hs
@@ -1,28 +1,29 @@
 module Euler where
 import Data.List
+import Data.Char (digitToInt)
 
 multipleOf3Or5 :: Integer -> Bool
 multipleOf3Or5 a =
     if (a `mod` 3 == 0) || (a `mod` 5 == 0)
-        then True
-        else False
+    then True
+    else False
 
 multipleOf17Or19 :: Integer -> Bool
 multipleOf17Or19 a =
     if (a `mod` 17 == 0) || (a `mod` 19 == 0)
-        then True
-        else False
+    then True
+    else False
 
 sumOfMultiPlesInAGivenRangeUnderACondition :: Integer -> Integer -> (Integer -> Bool) -> Integer
 sumOfMultiPlesInAGivenRangeUnderACondition first second predicate =
     if first > second
-        then 0
-        else foldl (+) 0 (filter predicate [first..second])
+    then 0
+    else foldl (+) 0 (filter predicate [first..second])
 
 fibonachi :: Integer -> Integer -> Integer ->[Integer]
 fibonachi first second limit =
     if first > second || limit <= first
-        then []
+    then []
     else [first] ++ fibonachi second (first + second) limit
 
 isEven :: Integer -> Bool
@@ -51,32 +52,34 @@ isPalindrome number = number == reverseInt number
 
 palindromeFinder :: Integer -> Integer -> Integer -> [Integer] -> [Integer]
 palindromeFinder first second limit palindromeList =
-                                               if first == 1 && second == 1
-                                               then palindromeList
-                                               else if second == 1 && isPalindrome potentialPalindrome
-                                               then palindromeFinder (first-1) limit limit (palindromeList ++ [potentialPalindrome])
-                                               else if second == 1
-                                               then palindromeFinder (first-1) limit limit palindromeList
-                                               else if isPalindrome potentialPalindrome
-                                               then palindromeFinder first (second-1) limit (palindromeList ++ [potentialPalindrome])
-                                               else palindromeFinder first (second-1) limit palindromeList
-                                               where potentialPalindrome = first * second
+    if first == 1 && second == 1
+    then palindromeList
+    else if second == 1 && isPalindrome potentialPalindrome
+    then palindromeFinder (first-1) limit limit (palindromeList ++ [potentialPalindrome])
+    else if second == 1
+    then palindromeFinder (first-1) limit limit palindromeList
+    else if isPalindrome potentialPalindrome
+    then palindromeFinder first (second-1) limit (palindromeList ++ [potentialPalindrome])
+    else palindromeFinder first (second-1) limit palindromeList
+    where potentialPalindrome = first * second
 
 largestPalindromeProduct :: Integer -> Integer
 largestPalindromeProduct number = head (reverse (sort (palindromeFinder number number number [])))
 
 ghc :: Integer -> Integer -> Integer
-ghc number1 number2 = if number2 == 0
-                      then number1
-                      else ghc number2 (number1 `mod` number2)
+ghc number1 number2 =
+    if number2 == 0
+    then number1
+    else ghc number2 (number1 `mod` number2)
 
 lcm :: Integer -> Integer -> Integer
 lcm number1 number2 = round ((fromInteger (number1 * number2)) / (fromInteger (ghc number1 number2)))
 
 smallestNumberDivisibleByRange :: Integer -> Integer -> Integer
-smallestNumberDivisibleByRange limit initial = if limit == 1
-                                           then initial
-                                           else smallestNumberDivisibleByRange (limit-1) (Prelude.lcm limit initial)
+smallestNumberDivisibleByRange limit initial =
+    if limit == 1
+    then initial
+    else smallestNumberDivisibleByRange (limit-1) (Prelude.lcm limit initial)
 
 squereAndSum :: Integer -> Integer -> Integer
 squereAndSum total number = total + (number * number)
@@ -93,5 +96,21 @@ diffOfSumOfSquereAndSquereOfSum number = (squereOfSum number) - (sumOfSquere num
 
 nThPrime :: Integer -> Integer
 nThPrime number = (sieve [2..]) !! fromIntegral (number-1)
+
+lastN :: Int -> [a] -> [a]
+lastN n xs = drop (length xs - n) xs
+
+toInt :: [Char] -> [Integer]
+toInt string = map fromIntegral (map digitToInt string)
+
+productOfNNumbers :: Integer -> Integer -> [Integer] -> Integer
+productOfNNumbers len multVal list =
+    if (length list) < (fromIntegral len)
+    then multVal
+    else if newVal > multVal
+    then productOfNNumbers len newVal (tail list)
+    else productOfNNumbers len multVal (tail list)
+    where newVal = foldl (*) 1 (lastN (fromIntegral len) (reverse list))
+
 
 
--- eulerTest.hs
+++ eulerTest.hs
@@ -7,6 +7,27 @@ import Euler
 
 main :: IO ()
 main = hspec $ do
+    let bigNum =  "73167176531330624919225119674426574742355349194934" ++
+                  "96983520312774506326239578318016984801869478851843" ++
+                  "85861560789112949495459501737958331952853208805511" ++
+                  "12540698747158523863050715693290963295227443043557" ++
+                  "66896648950445244523161731856403098711121722383113" ++
+                  "62229893423380308135336276614282806444486645238749" ++
+                  "30358907296290491560440772390713810515859307960866" ++
+                  "70172427121883998797908792274921901699720888093776" ++
+                  "65727333001053367881220235421809751254540594752243" ++
+                  "52584907711670556013604839586446706324415722155397" ++
+                  "53697817977846174064955149290862569321978468622482" ++
+                  "83972241375657056057490261407972968652414535100474" ++
+                  "82166370484403199890008895243450658541227588666881" ++
+                  "16427171479924442928230863465674813919123162824586" ++
+                  "17866458359124566529476545682848912883142607690042" ++
+                  "24219022671055626321111109370544217506941658960408" ++
+                  "07198403850962455444362981230987879927244284909188" ++
+                  "84580156166097919133875499200524063689912560717606" ++
+                  "05886116467109405077541002256983155200055935729725" ++
+                  "71636269561882670428252483600823257530420752963450"
+
     describe "multipleOf3Or5" $ do
 
         it "should give True for 3" $ do
@@ -111,25 +132,33 @@ main = hspec $ do
     describe "sumOfSquere" $ do
 
         it "shiuld give 385 for 10" $ do
-        sumOfSquere 10 `shouldBe` 385
+            sumOfSquere 10 `shouldBe` 385
 
     describe "squereOfSum" $ do
 
         it "shiuld give 3025 for 10" $ do
-        squereOfSum 10 `shouldBe` 3025
+            squereOfSum 10 `shouldBe` 3025
 
     describe "diffOfSumOfSquereAndSquereOfSum" $ do
 
         it "should give 2640 for 10" $ do
-        diffOfSumOfSquereAndSquereOfSum 10 `shouldBe` 2640
+            diffOfSumOfSquereAndSquereOfSum 10 `shouldBe` 2640
 
---        it "should give 25164150 for 100" $ do
---        diffOfSumOfSquereAndSquereOfSum 100 `shouldBe` 25164150
+        it "should give 25164150 for 100" $ do
+            diffOfSumOfSquereAndSquereOfSum 100 `shouldBe` 25164150
 
     describe "nThPrime" $ do
 
---        it "should give 13 for 6" $ do
---        nThPrime 6 `shouldBe` 13
+        it "should give 13 for 6" $ do
+            nThPrime 6 `shouldBe` 13
 
         it "should give 104743 for 10001" $ do
-        nThPrime 10001 `shouldBe` 104743
+            nThPrime 10001 `shouldBe` 104743
+
+    describe "productOfNNumbers" $ do
+
+        it "should give 5832 for given string and 4" $ do
+            productOfNNumbers 4 0 (toInt bigNum) `shouldBe` 5832
+
+        it "should give 23514624000 for given string and 13" $ do
+            productOfNNumbers 13 0 (toInt bigNum) `shouldBe` 23514624000
